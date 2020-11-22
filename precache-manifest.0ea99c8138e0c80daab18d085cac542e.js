9123ecbcb16ac0965c156e090c982bd91e9dbe0a
Evgeniy <evgeniy@MacBook-Pro-Evgeniy.local>
2020-11-22 02:06:09 +0200

    Updates

---
+++ precache-manifest.0ea99c8138e0c80daab18d085cac542e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927de5cf523705e68a06876bd45ba4af",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "c6fd6b22773dc7a68c6c",
+    "url": "/github-users/static/css/5.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1230b9aed877b155fa68",
+    "url": "/github-users/static/js/0.c3ffce9a.chunk.js"
+  },
+  {
+    "revision": "c6fd6b22773dc7a68c6c",
+    "url": "/github-users/static/js/5.92fa24d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github-users/static/js/5.92fa24d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af1c32a7a1b66dab2de",
+    "url": "/github-users/static/js/6.35a1328f.chunk.js"
+  },
+  {
+    "revision": "8c4e33c88f4bfb9fb521",
+    "url": "/github-users/static/js/7.fc017346.chunk.js"
+  },
+  {
+    "revision": "6be76d86591cd90053c2",
+    "url": "/github-users/static/js/UserProfile.5b19169e.chunk.js"
+  },
+  {
+    "revision": "b3ab11db6dd382b73234",
+    "url": "/github-users/static/js/UsersList.33feabfc.chunk.js"
+  },
+  {
+    "revision": "89e2a963f891b381a854",
+    "url": "/github-users/static/js/main.4c2d5e00.chunk.js"
+  },
+  {
+    "revision": "efdbc535337f5b22711e",
+    "url": "/github-users/static/js/runtime-main.a017a680.js"
+  }
+]);
